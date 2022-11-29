55d46de66b1e5ea5fe75e7556658a536c3b77feb
Margaux Forget <forgetmargaux@gmail.com>
2022-11-29 17:39:52 +0100

    add PIL to requierements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if "git+" not in x]
 
-setup(name='movies',
+setup(name='moviespred',
       version="0.0.4",
       description="Movies bill prediction",
       license="MIT",
