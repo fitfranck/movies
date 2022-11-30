d3e30252d752962f90a32d36277257b3030a589f
franck fitoussi <fitfranc@gmail.com>
2022-11-30 15:05:46 +0100

    save wip

--- moviespred/data.py
+++ moviespred/data.py
@@ -4,6 +4,10 @@ import os
 from moviespred import paths
 from moviespred.preprocessing import resize_image
 import pandas as pd
+import requests as rq
+from PIL import Image
+import io
+from moviespred import paths
 
 genre_liste =['action',
  'adventure',
@@ -46,9 +50,10 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     )
 
 def upload_images(genre, source_dir, target_blob):
-
+    #genre : genre_name
+    #source_dir : source de l'upload
+    #target_blob : où les mettre
     images_list = list(Path(source_dir).glob(f'{genre.lower()}/*.jpg'))
-
     images_files = [str(path) for path in images_list]
 
     for img in images_files:
@@ -58,10 +63,82 @@ def upload_images(genre, source_dir, target_blob):
         upload_blob(BUCKET, path_to_image, f'{target_blob}/{genre.lower()}/{image_name}')
 
 
-def download_images(genre, nbr):
-    for i in range nbr:
-        if not i%10:
-        fct telecharger
-        fct resized
-        fct uload BUCKET
-        rm
+
+def movies_pred(genre):
+
+    genres_raw = [
+    {'id': 28, 'name': 'action'},
+    {'id': 12, 'name': 'adventure'},
+    {'id': 16, 'name': 'animation'},
+    {'id': 35, 'name': 'comedy'},
+    {'id': 80, 'name': 'crime'},
+    {'id': 99, 'name': 'documentary'},
+    {'id': 18, 'name': 'drama'},
+    {'id': 10751, 'name': 'family'},
+    {'id': 14, 'name': 'fantasy'},
+    {'id': 36, 'name': 'history'},
+    {'id': 27, 'name': 'horror'},
+    {'id': 10402, 'name': 'music'},
+    {'id': 9648, 'name': 'mystery'},
+    {'id': 10749, 'name': 'romance'},
+    {'id': 878, 'name': 'science-fiction'},
+    {'id': 10770, 'name': 'tv-movie'},
+    {'id': 53, 'name': 'thriller'},
+    {'id': 10752, 'name': 'war'},
+    {'id': 37, 'name': 'western'}
+]
+
+    df = pd.read_csv(f'{paths["ref"]}/references.csv')
+
+    df = df.drop(columns = 'Unnamed: 0')
+    df = df.drop_duplicates()
+    df = df.dropna(subset=['overview','poster_path'])
+    df = df[df.is_principal != 10770]
+    df['id'] = df['id'].apply(lambda x: '{0:0>7}'.format(x))
+
+    df['poster_url'] = 'https://www.themoviedb.org/t/p/original/' + df['poster_path']
+
+    non_num_values = []
+
+    for i in df['id']:
+        if i.isdigit() == False:
+            non_num_values.append(i)
+
+    for i in non_num_values:
+        indexNames = df[ df['id'] == i ].index
+        df = df.drop(indexNames)
+
+    df = df.sort_values(by = 'popularity', ascending = False)
+
+    df_genre = df[df['is_principal'] == [x['id'] for x in genres_raw if x['name']==genre][0]]
+    df_genre = df_genre.sort_values(by = 'popularity', ascending = False)
+
+    list_address = df_genre['poster_url'].to_list()
+    list_names = df_genre['id'].to_list()
+
+    for content, save_name in zip(list_address, list_names):
+        res = rq.get(content)
+        save_path = f"{paths['raw_images']}/{genre}/{save_name}.jpg"
+        with open(save_path, 'w') as f:
+            image = Image.open(io.BytesIO(res.content))
+            image.save(f)
+            print(image)
+
+def remove(source_dir,genre):
+    dir_ = f'{paths["raw_images"]}/{genre}'
+    files = os.listdir(dir_)
+    for img in files:
+        os.remove(img)
+
+
+def download_images(genre):
+
+    movies_pred(genre)
+
+        # fct telecharger
+    resize_image(genre)
+        # fct resized
+    upload_images(genre, paths['raw_images', ])
+    upload_images(genre, paths['resize_images',])
+        # fct uload BUCKET
+    remove(genre)
--- moviespred/preprocessing.py
+++ moviespred/preprocessing.py
@@ -1,14 +1,16 @@
 from PIL import Image
 import os
+from moviespred import paths
 
-def resize_image(image_dir,target_size=(400,600)):
+def resize_image(genre,target_size=(400,600)):
 
-    for genre in genre_liste :
-        path_images = (f'../movies/raw_images/{genre}')
+    # for genre in image_dir :
+    # path_images_resize = paths['resize_images']
+    dir_ = f'{paths["raw_images"]}/{genre}'
+    files = os.listdir(dir_)
+    print(files)
 
-        files = os.listdir(path_images)
-
-        for img in files:
-            new_image = Image.open(f'raw_images/{genre}/{img}')
-            new_image = new_image.resize(target_size)
-            new_image.save(f'images_resized/{genre}/{img}')
+    for img in files:
+        new_image = Image.open(f'{dir_}/{img}')
+        new_image = new_image.resize(target_size)
+        new_image.save(f'{paths["resize_images"]}/{genre}/{img}')
