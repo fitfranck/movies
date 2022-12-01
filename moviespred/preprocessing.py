cccdf462db0578b1092da32279b8834506f448bc
Margaux Forget <forgetmargaux@gmail.com>
2022-12-01 10:59:20 +0100

    add to_rgb to preproc

--- moviespred/preprocessing.py
+++ moviespred/preprocessing.py
@@ -1,6 +1,8 @@
 from PIL import Image
-import os
+import numpy as np
+from moviespred.references import genres
 from moviespred import paths
+import os
 
 def resize_image(genre,target_size=(400,600)):
 
@@ -14,3 +16,13 @@ def resize_image(genre,target_size=(400,600)):
         new_image = Image.open(f'{dir_}/{img}')
         new_image = new_image.resize(target_size)
         new_image.save(f'{paths["resize_images"]}/{genre}/{img}')
+
+
+def to_rgb(genre):
+    files = os.listdir(f'{paths["resize_images"]}/{genre}')
+    for i in range(len(files)):
+        img = np.array(Image.open(f'{paths["resize_images"]}/{genre}/{files[i]}'))
+        if len(img.shape) == 2:
+            img = np.repeat(img[:,:,None], 3, axis=2)
+            im = Image.fromarray(img)
+            im.save(f'{paths["resize_images"]}/{genre}/{files[i]}')
