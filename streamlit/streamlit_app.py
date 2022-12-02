74f96ee3afe25846bef8aeb07986804c65c8e35a
franck fitoussi <fitfranc@gmail.com>
2022-12-02 17:43:10 +0100

    update

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -12,3 +12,57 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+
+
+if st.checkbox('Show content'):
+    st.write('''
+        This code will only be executed when the check box is checked
+
+        Streamlit elements injected inside of this block of code will \
+        not get displayed unless it is checked
+        ''')
+
+
+
+if st.button('More 🎈🎈🎈 please!'):
+    st.balloons()
+
+
+
+import base64
+
+@st.cache
+def load_image(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+    return encoded
+
+def image_tag(path):
+    encoded = load_image(path)
+    tag = f'<img src="data:image/png;base64,{encoded}">'
+    return tag
+
+def background_image_style(path):
+    encoded = load_image(path)
+    style = f'''
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{encoded}");
+        background-size: cover;
+    }}
+    </style>
+    '''
+    return style
+
+
+image_path = 'images/python.png'
+image_link = 'https://docs.python.org/3/'
+
+st.write('*Hey*, click me I\'m a button!')
+
+st.write(f'<a href="{image_link}">{image_tag(image_path)}</a>', unsafe_allow_html=True)
+
+if st.checkbox('Show background image', False):
+    st.write(background_image_style(image_path), unsafe_allow_html=True)
