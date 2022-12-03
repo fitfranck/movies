f2d6aac80c829b5fd2452d924f35eeb70a035709
franck fitoussi <fitfranc@gmail.com>
2022-12-03 18:05:26 +0100

    111

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+import io
+from PIL import Image
 # from moviespred.references import paths
 
 
@@ -75,3 +76,10 @@ if file_jpg:
 
 # if st.checkbox('Show background image', False):
 #     st.write(background_image_style(image_path), unsafe_allow_html=True)
+uploaded_files = st.file_uploader("Veuillez charger une image",type=['jpg','jpeg','png'],help="Charger une image au format jpg,jpeg,png", accept_multiple_files=True,)
+
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     image = Image.open(io.BytesIO(bytes_data))
+     st.write("filename:", uploaded_file.name)
+     st.image(image)
