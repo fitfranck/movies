bb5584b7c02831916639119a708e40499d671c07
franck fitoussi <fitfranc@gmail.com>
2022-12-05 16:27:03 +0100

    solve

--- interface/main.py
+++ interface/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from tensorflow.keras.models import load_model
 from moviespred import paths
 import os
 import numpy as np
+
+import io
+from PIL import Image
+
+
 # from tensorflow.keras import  models
 
 app = FastAPI()
@@ -13,10 +18,21 @@ app.state.model = load_model(os.path.join(paths['models'], 'full_model'))
 async def root():
     return {"status": "UP"}
 
-@app.get('/predict')
-def predict():
-    # Fake image to test model load and predict
-    image = np.ones(shape=(1, 600, 400, 3))
+@app.post('/predict')
+async def predict(img: UploadFile=File(...)):
+
+    contents = await img.read()
+    if contents:
+
+        image = Image.open(io.BytesIO(contents))
+        image = image.resize((400,600))
+        image = np.array(image)
+        image = image.reshape((1,600,400,3))
+
+
+    else:
+        # Fake image to test model load and predict
+        image = np.ones(shape=(1, 600, 400, 3))
     # yet to to code, remaining question : are we going with a recording or an image directly?
     pred = app.state.model.predict(image)
     return {"prediction": str(pred)}  #transform np arry et que je reshape
--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,84 +1,9 @@
 import streamlit as st
-import pandas as pd
-import io
 from PIL import Image
 import requests as rq
 
 
-
-st.write    ("""
-                # **IMAGE-IN**
-
-        ## *What's on the bill tonight guys  ?*
-
-
-""")
-
-
-uploaded_files = st.file_uploader("drag and drop movies'posters",type=['jpg','jpeg','png'],help="Charger une image au format jpg,jpeg,png", accept_multiple_files=True,)
-
-for uploaded_file in uploaded_files:
-     bytes_data = uploaded_file.read()
-     image = Image.open(io.BytesIO(bytes_data))
-    #  st.write("filename:", uploaded_file.name)
-     st.image(image)
-
-
-
-
-image_in_api_url = 'http://127.0.0.1:8000/predict'
-
-response = rq.get(image_in_api_url)
-
-prediction = response.json()
-
-pred = prediction['genre']
-
-st.header(f"the movie's genre: ${round(pred, 2)}")
-
-
-if st.button("it's true "):
-    st.balloons()
-
-
-
-
-
-
-
-
-# from moviespred.references import paths
-
-
-# df = pd.DataFrame({
-#   'first column': [1, 2, 3, 4],
-#   'second column': [10, 20, 30, 40]
-# })
-
-# df
-# st.write("WHAT's on the bill tonight guys ?")
-
-
-
-# st.write("how it's work ?")
-
-# st.write("just drag and drop movies'posters and click on them for know their genres")
-
-
-
-# if st.checkbox('Show content'):
-#     st.write('''
-#         This code will only be executed when the check box is checked
-
-#         Streamlit elements injected inside of this block of code will \
-#         not get displayed unless it is checked
-#         ''')
-
-
-
-
-
-
+url = 'http://localhost:8000'
 
 
 
@@ -94,32 +19,93 @@ if st.button("it's true "):
 #     tag = f'<img src="data:image/png;base64,{encoded}">'
 #     return tag
 
-# def background_image_style(path):
-#     encoded = load_image(path)
-#     style = f'''
-#     <style>
-#     .stApp {{
-#         background-image: url("data:image/png;base64,{encoded}");
-#         background-size: cover;
-#     }}
-#     </style>
-#     '''
-#     return style
 
-# file_jpg = st.file_uploader("Upload a PNG image", type=([".jpg"]))
+# st.write    ("""
+#                 # **IMAGE-IN**
 
-# if file_jpg:
-#     file_jpg_bytes = st.file_reader(file_jpg)
-#     st.image(file_jpg_bytes)
+#         ## *What's on the bill tonight guys  ?*
+
+
+# """)
+
+# original_title = '<p style="font-family:Courier; color:Pink; font-size: 50px;"> IMAGE-IN</p>'
+# st.write(original_title, unsafe_allow_html=True)
 
 
 
-# image_path = paths['image_train']/'Action'/'1008779.jpg'
-# # image_link = 'https://docs.python.org/3/'
+# CSS = """
+# h1 {
+#     color: red;
+# }
+# .stApp {
+#     background-image: url(https://avatars1.githubusercontent.com/u/9978111?v=4);
+#     background-size: cover;
+# }
+# """
 
-# st.write('*Hey*, click me I\'m a button!')
 
-# st.write(f'<a href="{image_tag(image_path)}</a>', unsafe_allow_html=True)
+# st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-# if st.checkbox('Show background image', False):
-#     st.write(background_image_style(image_path), unsafe_allow_html=True)
+
+st.write    ("""
+                # **IMAGE-IN**
+
+        ## *What's on the bill tonight guys  ?*
+
+
+""")
+
+uploaded_files = st.file_uploader("drag and drop movies'posters",type=['jpg','jpeg','png'],help="Charger une image au format jpg,jpeg,png", accept_multiple_files=True,)
+
+
+
+for uploaded_file in uploaded_files:
+    image = Image.open(uploaded_file)
+    image = image.resize((400,600))
+
+    st.image(image, caption="Here's the image you uploaded ☝️")
+    img_bytes = uploaded_file.getvalue()
+
+# if uploaded_files:
+
+clik = st.button('click')
+if clik:
+    st.write('coucou')
+    res = rq.post(url + "/predict", files={'img': img_bytes})
+    st.write(res.json())
+if uploaded_files:
+    res = rq.post(url + "/predict", files={'img': img_bytes})
+    st.write('predict!')
+    st.write(res.status_code)
+    if res.status_code=='200':
+        st.write(f'<a href="{url}">{res}</a>', unsafe_allow_html=True)
+    else:
+        pass
+
+# if st.checkbox("Show the movies' genre", False):
+#     predict(image: UploadFile=File(f"{url}+/predict"))
+
+
+if st.button("it's true "):
+    st.balloons()
+# if res.status_code == 200:
+#     ### Display the image returned by the API
+#     st.image(res.content, caption="Image returned from API ☝️")
+# else:
+#     st.markdown("**Oops**, something went wrong 😓 Please try again.")
+#     print(res.status_code, res.content)
+# image_in_api_url = 'http://localhost:8000/predict'
+
+# response = rq.get(image_in_api_url)
+
+# prediction = response.json()
+
+# pred = prediction['genre']
+
+# st.header(f"the movie's genre: ${round(pred, 2)}")
+
+
+
+
+
+# image_path = 'images/python.png'
